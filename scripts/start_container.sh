ac2063d9315582b6610888ed4d74c8f0725987cb
manojgowda593 <manoj.gowda1593@gmail.com>
2025-01-20 16:48:45 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull manoj1593/python_app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 manoj1593/python_app
