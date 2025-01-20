45f5056484efacbc4bd8977e88b52df0059dd982
manojgowda593 <manoj.gowda1593@gmail.com>
2025-01-20 17:00:50 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull manoj1593/python_app
+docker pull manoj1593/python_app:v1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 manoj1593/python_app
+docker run -d -p 5000:5000 manoj1593/python_app:v1
